7a2003399d84323d1551caed4ac659e4d53e4942
Ricardo Cardenes <ricardo.cardenes@noirlab.edu>
2026-04-19 15:44:02 -0400

    Adds tests for the scanner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use clap::Parser;
 use acwj_rs::Scanner;
 
+#[derive(Parser, Debug)]
+struct Args {
+    file_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let file = std::fs::File::open(args.file_name).expect("Failed to open file");
+    let mut scanner = Scanner::new(file);
+
+    while let Some(token) = scanner.scan() {
+        println!("{:?}", token);
+    }
 }
--- src/scan.rs
+++ src/scan.rs
@@ -1,12 +1,12 @@
 use std::io::{BufReader, Read};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Plus,
     Minus,
     Star,
     Slash,
-    IntLiteral(i64),
+    IntLit(i64),
 }
 
 #[derive(Debug)]
@@ -34,7 +34,7 @@ where T: Read
             '*' => Some(Token::Star),
             '/' => Some(Token::Slash),
             c if c.is_digit(10) => {
-                Some(Token::IntLiteral(self.scanint(c)))
+                Some(Token::IntLit(self.scanint(c)))
             }
             _ => None,
         }
@@ -87,5 +87,68 @@ where T: Read
 
         value
     }
+}
 
+struct ScannerIter<T> {
+    scanner: Scanner<T>,
+}
+
+impl<T> ScannerIter<T> {
+    fn new(scanner: Scanner<T>) -> Self {
+        ScannerIter { scanner }
+    }
+}
+
+impl <T> Iterator for ScannerIter<T>
+where T: Read
+{
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.scanner.scan()
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use std::path::PathBuf;
+    use rstest::rstest;
+        // assert_eq!(tokens, expected);
+
+    use super::*;
+
+    static TEST_DATA_FILES: &[&str] = &[
+        "input01",
+        "input02",
+        "input03",
+        "input04",
+        // "input05",
+    ];
+
+    static EXPECTED_TOKENS: &[&[Token]] = &[
+        &[Token::IntLit(2), Token::Plus, Token::IntLit(3), Token::Star, Token::IntLit(5), Token::Minus, Token::IntLit(8), Token::Slash, Token::IntLit(3)],
+        &[Token::IntLit(13), Token::Minus, Token::IntLit(6), Token::Plus, Token::IntLit(4), Token::Star, Token::IntLit(5), Token::Plus, Token::IntLit(8), Token::Slash, Token::IntLit(3)],
+        &[Token::IntLit(12), Token::IntLit(34), Token::Plus, Token::Minus, Token::IntLit(56), Token::Star, Token::Slash, Token::Minus, Token::Minus, Token::IntLit(8), Token::Plus, Token::Star, Token::IntLit(2)],
+        &[Token::IntLit(23), Token::Plus, Token::IntLit(18), Token::Minus, Token::IntLit(45)],
+    ];
+
+    fn get_test_file(filename: &str) -> PathBuf {
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("resources/test/scanner")
+                                                 .join(filename)
+    }
+
+    #[rstest]
+    #[case::input01(get_test_file(TEST_DATA_FILES[0]), EXPECTED_TOKENS[0])]
+    #[case::input02(get_test_file(TEST_DATA_FILES[1]), EXPECTED_TOKENS[1])]
+    #[case::input03(get_test_file(TEST_DATA_FILES[2]), EXPECTED_TOKENS[2])]
+    #[case::input04(get_test_file(TEST_DATA_FILES[3]), EXPECTED_TOKENS[3])]
+    fn test_scanner(#[case] file: PathBuf, #[case] expected: &[Token]) {
+        let file = std::fs::File::open(file).expect("Failed to open test file");
+        let scanner = Scanner::new(file);
+
+        let tokens: Vec<Token> = ScannerIter::new(scanner).into_iter().collect();
+
+        assert_eq!(tokens, expected.to_vec());
+    }
 }
