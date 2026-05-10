e1d2b6cae63f98334c562464358f97f4fc5d21d7
Ricardo Cárdenes <ricardo.cardenes@gmail.com>
2026-05-10 17:35:09 -0400

    Remove the early interpreter now that we're producing code

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@ pub mod ast;
 pub mod cg;
 pub mod expr;
 pub mod cgen;
-pub mod interp;
 pub mod scan;
 pub mod stmt;
 pub mod tree;
 
 pub use scan::{Scanner, Token};
 pub use expr::binexpr;
-pub use interp::interpret_ast;
